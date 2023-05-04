708db2b1b2be0ac65e45416cef97dba3285bb942
ImamSR <95268127+ImamSR@users.noreply.github.com>
2023-05-04 12:39:34 +0700

    Add files via upload

---
+++ inheritance/SecondCharac.java
@@ -0,0 +1,17 @@
+public class SecondCharac extends Character {
+    private int physicalDamage;
+
+    public SecondCharac (int HP, int basicAttack, int skillDamage, int physicalDamage){
+        super(HP, basicAttack, skillDamage);
+        this.physicalDamage = physicalDamage;
+    }
+
+    public int getMovementSpeed(){
+        return physicalDamage;
+    }
+
+    public void ultimateSkill(){
+        System.out.println("Aku akan menusuk hingga jantungmu!");
+    }
+
+}
