3f81bad1540f1e92ab8264c6046ed03cd988ca42
ImamSR <95268127+ImamSR@users.noreply.github.com>
2023-05-04 11:52:19 +0700

    Add files via upload

---
+++ Inharitence/MainCharac.java
@@ -0,0 +1,16 @@
+public class MainCharac extends Character {
+    private int magicDamage;
+
+    public MainCharac (int HP, int basicAttack, int skillDamage, int magicDamage){
+        super(HP, basicAttack, skillDamage);
+        this.magicDamage = magicDamage;
+    }
+
+    public int getMagicDamage(){
+        return magicDamage;
+    }
+
+    public void skillUse(){
+        System.out.println("Payungku akan menghancurkanmu!");
+    }
+}
